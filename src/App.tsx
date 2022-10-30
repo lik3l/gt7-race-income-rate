4ee89078ae507089b211cceeba662c5b88de7c87
Ihor Magur <ihor.magur@devabit.com>
2022-10-30 14:45:07 +0200

    Added MUI, initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
+import { Dashboard } from './layouts/Dashboard/Dashboard'
+
+const theme = createTheme({})
 
 const App: React.FC = () => {
-  return <div>Init</div>
+  return <React.Fragment>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Dashboard />
+    </ThemeProvider>
+  </React.Fragment>
 }
 
 export default App
