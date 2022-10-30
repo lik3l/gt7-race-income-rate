a869461f6d5018e6dd11b5d5cbbec3b2d1b5aba8
Ihor Magur <ihor.magur@devabit.com>
2022-10-30 14:47:07 +0200

    Added welcome

--- src/layouts/Dashboard/Dashboard.tsx
+++ src/layouts/Dashboard/Dashboard.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import styled from '@emotion/styled'
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`
 
 export const Dashboard: React.FC = () => {
   return (
-    <div>
-      Hello
-    </div>
+    <Wrapper>
+      <h1>Hello, Guest</h1>
+    </Wrapper>
   )
 }
